d6f106e1059c1d009561e1aaf8a60fad8caa22ad
wankhede04 <wankhedevijay04@gmail.com>
2020-11-02 21:22:28 +0530

    Integrated payment service.

---
+++ src/app/services/payment.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ICardDetails, IHttpResponse } from '../models/card-details.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  constructor(private http: HttpClient) {}
+
+  public createPayment(payload: ICardDetails): Observable<IHttpResponse> {
+    return this.http.post<IHttpResponse>(
+      `${environment.serverUrl}`,
+      payload
+    );
+  }
+}
