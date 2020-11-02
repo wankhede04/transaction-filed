a536b9610215373ad2b149828301c8092bc6b56a
wankhede04 <wankhedevijay04@gmail.com>
2020-11-02 21:30:55 +0530

    Defined and implemented route for components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardDetailsComponent } from './card-details/card-details.component';
+import { RedirectButtonComponent } from './redirect-button/redirect-button.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RedirectButtonComponent },
+  { path: 'card-detail', component: CardDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CardDetailsComponent } from './card-details/card-details.component';
+import { RedirectButtonComponent } from './redirect-button/redirect-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardDetailsComponent,
+    RedirectButtonComponent
   ],
   imports: [
     BrowserModule,
